e537cc121ff8f2e5d74c8176e8c233ebab2a08ba
David's Devel <davidsdevel@gmail.com>
2023-06-21 20:34:03 -0430

    feat: Added user invitation modal

--- apps/main/components/dashboard/contacts/index.jsx
+++ apps/main/components/dashboard/contacts/index.jsx
@@ -1,11 +1,20 @@
 import {useMemo} from 'react';
-import {useUser} from '@/components/dashboard/context';
+import {useUser, useSocket} from '@/components/dashboard/context';
 import {BsPlus} from 'react-icons/bs';
 import ContactTemplate from './contactTemplate';
 import ContactLoader from './contactLoader';
 
+import Modal from '@/components/modal';
+import Input from '@/components/input';
+import Button from '@/components/button';
+
 export default function Messages() {
+  const [show, setShow] = useState(false);
+  const [friendID, setFriendID] = useState('');
+
   const {loading, user} = useUser();
+  const socket = useSocket();
+
 
   const sorted = useMemo(() => {
     if (user)
@@ -23,5 +32,30 @@ export default function Messages() {
     <button className='bg-gradient-to-br from-green-400 to-main-500 fixed bottom-4 right-4 rounded-full p-2'>
       <BsPlus className='text-white h-8 w-8'/>
     </button>
+
+    <Modal isOpen={show} close={() => setShow(false)}>
+      <div className='z-10 rounded-2xl bg-slate-100 px-4 py-8 flex flex-col'>
+        <Input type='text' placeholder='Friend ID' value={friendID} onChange={({target: {value}}) => setFriendID(value)}/>
+        <Button onClick={async () => {
+          const friend = await user.addFriend(friendID);
+
+          socket.emit('new-friend', {
+            from: user._id,
+            to: friendID
+          });
+
+          onAddFriend(friend);
+          setFriendID('');
+          setShow(false);
+        }}>Add Contact</Button>
+        <hr className='my-4'/>
+        <Input type='text' disabled value={user._id}/>
+        <Button onClick={() => {
+          navigator.clipboard.writeText(user._id);
+
+          alert('Copiado');
+        }}>Copy ID</Button>
+      </div>
+    </Modal>
   </div>;
 }
